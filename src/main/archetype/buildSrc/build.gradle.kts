3b8c2d4081a9dfd4e8ec70a50a97d1bf88bafc42
Krystian Panek <krystian.panek@cognifide.com>
2020-08-03 23:44:25 +0200

    Better env defaults

--- src/main/archetype/buildSrc/build.gradle.kts
+++ src/main/archetype/buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ dependencies {
     implementation("com.cognifide.gradle:aem-plugin:14.1.3")
     implementation("org.apache.sling:org.apache.sling.caconfig.bnd-plugin:1.0.2")
     implementation("com.github.node-gradle:gradle-node-plugin:2.2.4")
-    implementation("com.cognifide.gradle:environment-plugin:1.0.0")
+    implementation("com.cognifide.gradle:environment-plugin:1.0.2")
     implementation("com.neva.gradle:fork-plugin:5.0.4")
 }
