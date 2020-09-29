3babefcdfc72bc87c110314b9a2a14ab6ae944c0
Krystian Panek <krystian.panek@cognifide.com>
2020-09-29 20:22:36 +0200

    Hosts editor

--- src/main/archetype/buildSrc/build.gradle.kts
+++ src/main/archetype/buildSrc/build.gradle.kts
@@ -10,6 +10,6 @@ dependencies {
     implementation("org.apache.sling:org.apache.sling.bnd.models:1.0.0")
 
     implementation("com.github.node-gradle:gradle-node-plugin:2.2.4")
-    implementation("com.cognifide.gradle:environment-plugin:1.0.4")
+    implementation("com.cognifide.gradle:environment-plugin:1.1.1")
     implementation("com.neva.gradle:fork-plugin:5.0.6")
 }
